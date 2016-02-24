ed49720b7a05b40ed90f97c2e4fb1cf5fdd3625e
Dmitrii Solovev <dimonnwc3@gmail.com>
2016-02-24 13:05:30 +0100

    Added dynamic generation links for assets in jade

--- server/middlewares/04-templates.js
+++ server/middlewares/04-templates.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const jade = require('jade');
-const templatesPath = require('config').server.templates;
 const path = require('path');
+const fs = require('mz/fs');
+const templatesPath = require('config').server.templates;
+
+let assetsPath = require('config').client.assets;
+assetsPath = path.join(assetsPath, './assets.json');
+
 const moment = require('moment');
 
 module.exports = function*(next) {
 
   let ctx = this;
+  let assets = yield fs.readFile(assetsPath, {encoding: 'utf-8'});
+  assets = JSON.parse(assets);
 
   this.state = {
+    assets: assets,
     // at the time of this middleware, user is unknown, so we make it a getter
     get user() {
       return ctx.req.user; // passport sets this
